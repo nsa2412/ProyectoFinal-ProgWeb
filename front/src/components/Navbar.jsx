68cfcbb09788ef03368c19cb4e5b9a482e48739e
nsa2412 <ale.nicolas.salomon@gmail.com>
2023-10-16 11:27:24 -0300

    crada navbar

---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import default function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Generic Bank</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Quienes somos</Nav.Link>
+            <Nav.Link href="#link">Log in</Nav.Link>
+            <Nav.Link href="#link">Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
